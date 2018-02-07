80343b0a0374f9426c749c8236d2608a74be1645
Dao Vu Dat <dalmvicky@gmail.com>
2018-02-07 22:47:24 +0700

    add scene Add Card, DetailList - Use stack, scene - styling button using TouchableHighlight

--- App.js
+++ App.js
@@ -1,82 +1,36 @@
 import React, {Component} from 'react';
-import {Text, TouchableOpacity, View ,StyleSheet, FlatList} from 'react-native';
+import {Router, Stack, Scene} from 'react-native-router-flux';
+import List from './src/List';
+import DetailList from './src/DetailList';
+import AddCard from './src/AddCard';
+import Button from './src/components/Button'
+import {Alert,View} from 'react-native';
 
 export default class FlashCard extends Component {
+
+  onButtonPress(){
+    Alert.alert(
+      'Alert!!!',
+      'Pressed button',
+      [{text: "OK", onPress: ()=> console.log("OK, pressed!!!")}],
+      {cancelable:false}
+    )
+  }
+
   render(){
     return (
-      <View style = {styles.container}>
-         {/*Header*/}
-          <View style = {styles.header}>
-            {/*Header Text Left*/}
-            <View style = {styles.headerText}>
-              <Text style={styles.textHeader}> Custom List Flash Card </Text>
-              <Text style={styles.textHeader}> Cards: 10 </Text>
-            </View>
-
-            {/*Header Button Right*/}  
-            <View style = {styles.headerBtn}>          
-              <TouchableOpacity>
-                <Text style ={styles.textHeader}>
-                  Learn
-                </Text>
-              </TouchableOpacity>
-              <TouchableOpacity>
-                <Text style ={styles.textHeader}>
-                  Add Card
-                </Text>
-              </TouchableOpacity>
-            </View>
-          </View>
-          
-          {/*Header*/}
-          <View style = {styles.list}>
-            <FlatList
-              data={[
-                {key: 'Devin'},
-                {key: 'Jackson'},
-                {key: 'James'},
-                {key: 'Joel'},
-                {key: 'John'},
-                {key: 'Jillian'},
-                {key: 'Jimmy'},
-                {key: 'Julie'},
-              ]}
-              renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
-            />
-          </View>
+      // <Router>
+      //   <Stack key="root">
+      //     <Scene key="list" component={List} hideNavBar = {true}/>
+      //     <Scene key="detailList" component={DetailList} hideNavBar = {true}/>
+      //     <Scene key="addCard" component={AddCard} hideNavBar={true}/>
+      //   </Stack>
+      // </Router>
+      <View style ={{margin:50,flexDirection: 'row'}}>
+        <Button onPress={this.onButtonPress.bind(this)}>
+          AddCard
+        </Button>
       </View>
     );
   };
 }
-
-const styles = StyleSheet.create({
-  container:{
-    flex: 1, 
-    justifyContent: 'center',
-  },
-  header:{
-    flex:1,
-    backgroundColor: '#000',
-    padding: 10,
-    flexDirection: 'row',
-  },
-  headerText:{
-    flex:3,
-    margin:5,
-    justifyContent:'center',
-  },
-  headerBtn:{
-    flex:1,
-    margin: 5,
-    justifyContent:'center',
-    alignItems: 'flex-end',
-  },
-  list:{
-    flex:4,
-    backgroundColor:'#FFF',
-  },
-  textHeader:{
-    fontSize: 20,
-    color:'#FFF'
-  }
-})
---
+++ src/DetailList.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import { View, Text } from 'react-native';
+
+
+class DetailList extends Component {
+    render(){
+        return (
+            <View>
+                <Text>Detail List</Text>
+            </View>
+        );
+    };
+}
+
+export default DetailList;
