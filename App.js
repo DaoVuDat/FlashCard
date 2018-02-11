99a0d4645699b3fcc9bdeb4db01f3bcf362cfbf7
Dao Vu Dat <dat.daovu@gmail.com>
2018-02-11 23:58:23 +0700

    original flat list

--- App.js
+++ App.js
@@ -4,17 +4,90 @@ import List from './src/List';
 import CustomLearning from './src/CustomLearning';
 import EditCard from './src/EditCard';
 
+import CustomCard from './src/components/common/CustomCard'
+import {Text, FlatList, View} from 'react-native'
+
+const dataSource =[
+  {
+    key:"1",
+    en:"Hello",
+    sentence:"Hello. How are you?",
+    vn:"Xin chao",
+  },
+  {
+    key:"2",
+    en:"Hi",
+    sentence:"Hi. How are you?",
+    vn:"Xin chao",
+  },
+  {
+    key:"3",
+    en:"Fine",
+    sentence:"I am fine",
+    vn:"Tot",
+  },
+  {
+    key:"4",
+    en:"Good",
+    sentence:"I am good",
+    vn:"Gioi",
+  },
+  {
+    key:"5",
+    en:"Bad",
+    sentence:"I am bad",
+    vn:"Te",
+  },
+  {
+    key:"6",
+    en:"Go",
+    sentence:"I go to school",
+    vn:"Di",
+  },
+  {
+    key:"7",
+    en:"Fastest",
+    sentence:"I am the fastest man alive. I am the flash. I am the fastest man alive. I am the flash",
+    vn:"Nhanh nhat",
+  },
+  {
+    key:"8",
+    en:"Gun",
+    sentence:"I have a really big gun in my pocket",
+    vn:"Sung",
+  },
+]
+
+
+
 export default class FlashCard extends Component {
+
+  _keyExtractor = (item, index) => item.key;
+
+  _renderListItem = ({item}) =>{
+    return (
+       <CustomCard item={item} />
+      
+    )
+  }
 
   render(){
     return (
-      <Router>
-        <Stack key="root">
-          <Scene key="list" component={List} hideNavBar = {true}/>
-          <Scene key="editCard" component={EditCard} hideNavBar = {true}/>
-          <Scene key="customLearning" component={CustomLearning} hideNavBar={true}/>
-        </Stack>
-      </Router>
+      // <Router>
+      //   <Stack key="root">
+      //     <Scene key="list" component={List} hideNavBar = {true}/>
+      //     <Scene key="editCard" component={EditCard} hideNavBar = {true}/>
+      //     <Scene key="customLearning" component={CustomLearning} hideNavBar={true}/>
+      //   </Stack>
+      // </Router>
+      <View style ={{flex:1, marginTop:22}}>
+        <FlatList
+          data={dataSource}
+          
+          keyExtractor={this._keyExtractor}
+          renderItem={this._renderListItem}
+        />
+      </View>
     );
   };
 }
--- src/components/common/CustomCard.js
+++ src/components/common/CustomCard.js
@@ -1,4 +1,41 @@
 import React, { Component } from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, StyleSheet} from 'react-native';
 
-export default class 
+export default class CustomCard extends Component {
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        console.log(this.props.item);
+        return(
+            <View style ={{flex:1}}>
+                <View style = {styles.container}>
+                    <Text>{this.props.item.key}</Text>
+                    <Text style={styles.text}>{this.props.item.en}</Text>
+                    <Text style={styles.textFocus}>{this.props.item.sentence}</Text>
+                </View>
+
+                <View style={{height:1, backgroundColor:'#000'}}/>
+            </View>
+        )
+    };
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        margin: 20,
+        borderWidth:1,
+        backgroundColor:'gray',
+        height: 100
+    },
+    textFocus:{
+        fontSize: 25,
+        fontFamily:'Ubuntu-Bold',
+    },
+    text:{
+        fontSize: 20,
+        fontFamily:'Ubuntu-Light',
+    }
+})
